1789196b9121d47a8bf071d6c45215fcd07d1363
saher <aldobaisaher@gmail.com>
2026-07-15 14:03:20 +0300

    update api url to production

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
-// src/config.js
 export const API_BASE_URL = window.location.hostname === 'localhost' 
   ? 'http://localhost:3000' 
-  : 'https://your-backend-name.onrender.com'; // سنضع رابط Render هنا لاحقاً
+  : 'https://humanitarian-cell-frontend.onrender.com';
