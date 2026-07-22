443509674cda0da27e4e8bfd6349bfad4109dfc3
saher <aldobaisaher@gmail.com>
2026-07-23 00:41:27 +0300

    add test map component and red box for debugging

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,11 @@
-// في ملف src/main.jsx
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
-
+\import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './App.css'
 import './index.css'
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <BrowserRouter>
-       <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-
-   
-  
+    <App />
+  </React.StrictMode>
 )
