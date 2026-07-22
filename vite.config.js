e70889b5b952b2b3115ab10993dcdbf852f05bc0
saher <aldobaisaher@gmail.com>
2026-07-23 00:16:48 +0300

    add test map component and red box for debugging

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000'
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    // تجنب أي تحذيرات بناء من إيقاف عملية النشر
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') return;
+        warn(warning);
+      }
     }
   }
-})
+});
