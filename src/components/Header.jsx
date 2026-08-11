ed85c74082d306fd19630c488eb3987576509d85
saher <aldobaisaher@gmail.com>
2026-08-11 21:47:28 +0300

    Fix Dockerfile for deployment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -135,7 +135,6 @@ const Header = () => {
                 </li>
               )}
 
-              // ابحثي عن قائمة الـ nav-links وأضيفي هذا الرابط
 <li className="nav-item">
   <Link className="nav-link" to="/partners">بوابة الشركاء</Link>
 </li>
